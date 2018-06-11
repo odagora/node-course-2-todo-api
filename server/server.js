d3cf561d875a7825df58c8af034fc57de7f7eba4
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-06-11 13:51:10 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 //Import express and body-parser libraries:
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//Import lodash library to use it in the update CRUD method:
+const _ = require('lodash');
 
 //Use of object destructuring ES6 to pull data as variables:
 var {mongoose} = require('./db/mongoose');
@@ -75,6 +78,35 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+//Route to UPDATE an individual resource:
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  //Creation of a variable that pulls only the content to be updated. We use the '.pick' method of the 'lodash' library (ref. https://lodash.com/docs/4.17.10#pick):
+  var body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  //Check if the completed field is set to 'true'. Then change the 'completedAt' field to the actual timestamp. We use the 'isBoolean' lodash method and verify if it's 'true':
+  if (_.isBoolean(body.completed) && body.completed) {
+    //If true, we change the 'completedAt' field to the actual timestamp using the 'getTime()' method (ref. https://www.w3schools.com/jsref/jsref_gettime.asp):
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    //We reset all values to 'false' and 'null':
+    body.completed = false;
+    body.completedAt = null;
+  }
+  //If success, we update the document with the 'findByIdAndUpdate' mongoose method. The first argument is the key, and the second is the options:
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send()
+  });
+})
 
 //Setting up the express server:
 app.listen(port, () => {
