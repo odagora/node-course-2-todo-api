3be62cc6ee6c3f2459f7fe049cd78ea87436691b
ogonzalez29 <oscar.d.gonzalez@gmail.com>
2018-06-19 18:22:09 -0500

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -256,3 +256,57 @@ describe('POST /users', () => {
         .end(done)
     });
 });
+
+//Testing POST /users/login:
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        //We use the second document created in the seed file that doesn't have a token:
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        //Next we test the token generation:
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+  it('should reject invalid login', (done) => {
+    var password = '123abc';
+    request(app)
+      .post('/users/login')
+      .send({
+        //We use the second document created in the seed file that doesn't have a token:
+        email: users[1].email,
+        password: password
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        //Next we test the token generation:
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
